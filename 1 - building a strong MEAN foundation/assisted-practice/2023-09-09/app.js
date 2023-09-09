a8cc188a767a434cf6a3113696aa91d7406adac7
Rafhael Barabas <rafhael_ab@hotmail.com>
2023-09-09 14:42:40 -0300

    added option to create new posts and delete them

--- 1 - building a strong MEAN foundation/assisted-practice/2023-09-09/app.js
+++ 1 - building a strong MEAN foundation/assisted-practice/2023-09-09/app.js
@@ -1,70 +1,88 @@
-// const getData = () => {
-//   return new Promise((resolve, reject) => {
-//     const xhr = new XMLHttpRequest();
-//     xhr.open("get", "https://jsonplaceholder.typicode.com/posts");
-//     xhr.responseText = "json";
-//     xhr.onload = () => {
-//       if (xhr.status >= 200 && xhr.status < 300) {
-//         const postsList = xhr.response;
-//         console.log(postsList);
-//         resolve(postsList);
-//       } else {
-//         reject(`Request error, having status: ${xhr.status}`);
-//       }
-//     };
-//     xhr.send();
-//   });
-// };
+let postIdCounter = 100;
+const postsResourceUrl = "https://jsonplaceholder.typicode.com/posts";
 
 const postTemplate = document.getElementById("post-item");
 const listElement = document.querySelector(".posts");
 const fetchBtn = document.getElementById("fetch-button");
-
-console.log("postTemplate", postTemplate);
-console.log("listElement", listElement);
-console.log("fetch", fetchBtn);
 
 const sendHttpRequest = (method, url, data) => {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     xhr.responseType = "json";
     xhr.open(method, url);
-
     xhr.onload = () => {
-      resolve(xhr.response);
+      if (xhr.status < 300) {
+        resolve(xhr.response);
+      } else {
+        reject(
+          `An error was found during perform request. Status code: ${xhr.status}`
+        );
+      }
     };
-    xhr.send();
+
+    if (data !== null && data !== undefined) {
+      const jsonData = JSON.stringify(data);
+      xhr.send(jsonData);
+    } else {
+      xhr.send();
+    }
+
+    xhr.onerror = (ev) =>
+      reject("Some error was found during perform request.");
   });
 };
 
-const fetchPosts = async () => {
-  const postsList = await sendHttpRequest(
-    "GET",
-    "https://jsonplaceholder.typicode.com/posts"
-  );
-  for (const post of postsList) {
-    const postElement = document.importNode(postTemplate.content, true);
+const deletePostCard = (postId) => {
+  const divElements = listElement.querySelectorAll("div");
 
-    postElement.querySelector("h2").textContent = post.title;
-    postElement.querySelector("p").textContent = post.body;
-    listElement.append(postElement);
+  for (div of divElements) {
+    if (div.id == postId) {
+      if (window.confirm("Are you sure?")) {
+        listElement.removeChild(div);
+      }
+    }
   }
 };
 
-const createPost = async (title, content) => {
-  const savePost = await sendHttpRequest(
-    "POST",
-    "https://jsonplaceholder.typicode.com/posts",
-    {
-      title,
-      body: content,
-      userId: Math.random(),
-    }
-  );
-
-  console.log(`Saved post id: ${savePost}`);
+const appendPostElement = (title, content, postId) => {
+  const postElement = document.importNode(postTemplate.content, true);
+  const postCardDiv = postElement.querySelector("div");
+  postCardDiv.setAttribute("id", postId);
+  postElement.querySelector("h2").textContent = title;
+  postElement.querySelector("p").textContent = content;
+  const deleteButton = postElement.querySelector("button");
+  deleteButton.addEventListener("click", () => deletePostCard(postId));
+  listElement.append(postElement);
 };
 
-createPost("Sample post", "dummy content");
+const fetchPosts = async () => {
+  const postsList = await sendHttpRequest("GET", postsResourceUrl);
+  for (const post of postsList) {
+    appendPostElement(post.title, post.body, post.id);
+  }
+};
+
+const createPost = async (title, body) => {
+  const userId = Math.floor(Math.random() * (1000 - 1 + 1)) + 1;
+  await sendHttpRequest("POST", postsResourceUrl, {
+    title,
+    body,
+    userId,
+  })
+    .then((res) => {
+      postIdCounter++;
+      appendPostElement(title, body, postIdCounter);
+    })
+    .catch((err) => window.alert(err));
+};
 
 fetchBtn.addEventListener("click", fetchPosts);
+
+const form = document.getElementById("post-form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const data = new FormData(event.target);
+  const { title, content } = Object.fromEntries(data.entries());
+  createPost(title, content);
+  setTimeout(() => event.target.reset(), 600);
+});
