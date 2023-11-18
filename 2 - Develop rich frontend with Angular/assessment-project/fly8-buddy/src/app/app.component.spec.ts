299cd59a7db1cb72e2208291268ea4098e0ceea1
rafhaelbarabas <rafhael_ab@hotmail.com>
2023-11-18 11:32:04 -0300

    replacing default page

--- 2 - Develop rich frontend with Angular/assessment-project/fly8-buddy/src/app/app.component.spec.ts
+++ 2 - Develop rich frontend with Angular/assessment-project/fly8-buddy/src/app/app.component.spec.ts
@@ -11,17 +11,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'fly8-buddy'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('fly8-buddy');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('fly8-buddy app is running!');
-  });
 });
